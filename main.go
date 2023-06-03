27f811268af3a624920fd7253263330ff256aa4d
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2023-06-03 18:20:05 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type Router struct {
-	Routes     map[string]map[string]http.HandlerFunc
-	staticPath string
+	Routes       map[string]map[string]http.HandlerFunc
+	staticPath   string
+	CookieManager CookieManager
 }
 
 type Context struct {
@@ -21,10 +23,77 @@ type Context struct {
 	staticPath     string
 }
 
-func Default() *Router {
-	return &Router{
+type Cookie struct {
+	Name     string
+	Value    string
+	Path     string
+	Domain   string
+	Expires  time.Time
+	Secure   bool
+	HttpOnly bool
+}
+
+type Session struct {
+	cookie *Cookie
+}
+
+func NewSession(cookie *Cookie) *Session {
+	return &Session{
+		cookie: cookie,
+	}
+}
+
+func (s *Session) Get(key string) string {
+	return s.cookie.Value
+}
+
+func (s *Session) Set(key, value string) {
+	s.cookie.Value = value
+}
+
+type CookieManager interface {
+	SetCookie(w http.ResponseWriter, cookie *Cookie)
+	GetCookie(r *http.Request, name string) (*http.Cookie, error)
+}
+
+type DefaultCookieManager struct{}
+
+func (cm *DefaultCookieManager) SetCookie(w http.ResponseWriter, cookie *Cookie) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     cookie.Name,
+		Value:    cookie.Value,
+		Path:     cookie.Path,
+		Domain:   cookie.Domain,
+		Expires:  cookie.Expires,
+		Secure:   cookie.Secure,
+		HttpOnly: cookie.HttpOnly,
+	})
+}
+
+func (cm *DefaultCookieManager) GetCookie(r *http.Request, name string) (*http.Cookie, error) {
+	return r.Cookie(name)
+}
+
+type RouterOptions struct {
+	CookieManager CookieManager
+}
+
+func Default(options ...RouterOptions) *Router {
+	r := &Router{
 		Routes: make(map[string]map[string]http.HandlerFunc),
 	}
+
+	if len(options) > 0 {
+		cookieManager := options[0].CookieManager
+		if cookieManager == nil {
+			cookieManager = &DefaultCookieManager{}
+		}
+		r.CookieManager = cookieManager
+	} else {
+		r.CookieManager = &DefaultCookieManager{}
+	}
+
+	return r
 }
 
 func (r *Router) GET(path string, handler http.HandlerFunc) {
@@ -36,7 +105,18 @@ func (r *Router) POST(path string, handler http.HandlerFunc) {
 }
 
 func (r *Router) Run(port string) {
-	http.ListenAndServe(port, r)
+	server := &http.Server{
+		Addr:         port,
+		Handler:      r,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+
+	server.SetKeepAlivesEnabled(true)
+
+	r.enableCaching()
+
+	server.ListenAndServe()
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -55,7 +135,7 @@ func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func HTML(status int, filename string, data interface{}, w http.ResponseWriter) {
+func (r *Router) HTML(status int, filename string, data interface{}, w http.ResponseWriter) {
 	tmpl, err := template.ParseFiles(filename)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -71,7 +151,7 @@ func HTML(status int, filename string, data interface{}, w http.ResponseWriter) 
 	w.WriteHeader(status)
 }
 
-func JSON(status int, data interface{}, w http.ResponseWriter) {
+func (r *Router) JSON(status int, data interface{}, w http.ResponseWriter) {
 	jsonData, err := json.Marshal(data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -148,11 +228,11 @@ func (r *Router) Static(filenames ...string) {
 	}
 }
 
-func Redirect(w http.ResponseWriter, req *http.Request, url string) {
+func (r *Router) Redirect(w http.ResponseWriter, req *http.Request, url string) {
 	http.Redirect(w, req, url, http.StatusFound)
 }
 
-func SaveFile(file multipart.File, header *multipart.FileHeader, path string) error {
+func (r *Router) SaveFile(file multipart.File, header *multipart.FileHeader, path string) error {
 	out, err := os.Create(path)
 	if err != nil {
 		return err
@@ -165,4 +245,35 @@ func SaveFile(file multipart.File, header *multipart.FileHeader, path string) er
 	}
 
 	return nil
+}
+
+func (r *Router) SetCookie(w http.ResponseWriter, cookie *Cookie) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     cookie.Name,
+		Value:    cookie.Value,
+		Path:     cookie.Path,
+		Domain:   cookie.Domain,
+		Expires:  cookie.Expires,
+		Secure:   cookie.Secure,
+		HttpOnly: cookie.HttpOnly,
+	})
+}
+
+func (r *Router) GetCookie(req *http.Request, name string) (*http.Cookie, error) {
+	return req.Cookie(name)
+}
+
+func (r *Router) SetSession(session *Session, key, value string) {
+	session.Set(key, value)
+}
+
+func (r *Router) GetSession(session *Session, key string) string {
+	return session.Get(key)
+}
+
+func (r *Router) enableCaching() {
+	r.GET("/*filepath", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
+		http.FileServer(http.Dir(r.staticPath)).ServeHTTP(w, req)
+	})
 }
